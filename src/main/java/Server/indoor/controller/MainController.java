628df9a41b2da3d9c74c1b5f129b3841f8732b4b
junseoks-99 <angec5645@naver.com>
2023-09-19 21:41:35 +0900

    Feat: 추가 "객체전송 test api"

---
+++ src/main/java/Server/indoor/controller/MainController.java
@@ -0,0 +1,31 @@
+package Server.indoor.controller;
+
+import Server.indoor.domain.Student;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Stack;
+
+@Slf4j
+@RestController
+@RequestMapping("/info")
+public class MainController {
+
+    @GetMapping("/test")
+    public Student test() {
+        return new Student("jang", 12);
+    }
+
+}
