878afa62f4625587affa41b6c9af5216070a94c3
junseoks-99 <angec5645@naver.com>
2023-09-19 21:33:04 +0900

    Initial commit

---
+++ src/test/java/Server/indoor/IndoorApplicationTests.java
@@ -0,0 +1,13 @@
+package Server.indoor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndoorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
