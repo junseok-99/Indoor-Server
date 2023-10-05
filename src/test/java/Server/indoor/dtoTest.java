1f8f3a1ea2aaa71a19aeef7cd441479a4d2ba57a
junseoks-99 <angec5645@naver.com>
2023-10-05 16:46:40 +0900

    Test: 추가 "dto 로직 테스트"

---
+++ src/test/java/Server/indoor/dtoTest.java
@@ -0,0 +1,38 @@
+package Server.indoor;
+
+import Server.indoor.dto.Space;
+import Server.indoor.dto.SpaceInfo;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class dtoTest {
+
+    @Test()
+    public void dtoTESTs() {
+        List<SpaceInfo> list = new LinkedList<>();
+
+        list.add(new SpaceInfo("구역 1", -1.54f, 2.96f, -6.86f, -1.39f));
+        list.add(new SpaceInfo("구역 2", -1.54f, 2.96f, -6.86f, -1.39f));
+        list.add(new SpaceInfo("구역 3", -1.54f, 2.96f, -6.86f, -1.39f));
+
+        Space space = new Space();
+        space.setSpaceInfos(list);
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("spaceInfo", list);
+
+        try {
+            String json;
+            json = new ObjectMapper().writeValueAsString(data);
+            System.out.println(json);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+}
