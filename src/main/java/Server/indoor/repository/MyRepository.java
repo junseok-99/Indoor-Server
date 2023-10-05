0be2b00f479b297f0716ff6df5bc1586be034668
junseoks-99 <angec5645@naver.com>
2023-10-05 21:21:46 +0900

    Refactor: 제거 "findId()"

--- src/main/java/Server/indoor/repository/MyRepository.java
+++ src/main/java/Server/indoor/repository/MyRepository.java
@@ -1,11 +1,11 @@
 package Server.indoor.repository;
 
+import Server.indoor.dto.ARObjectInfo;
 import Server.indoor.dto.SpaceInfo;
 
 import java.util.List;
 
 public interface MyRepository {
 
-    String findId(String id);
     List<SpaceInfo> findSpace(String spaceName);
 }
