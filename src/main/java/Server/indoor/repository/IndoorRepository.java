8eaf5af30c6b2e310fc9e28beb4fca1f09ae2736
junseoks-99 <angec5645@naver.com>
2023-10-05 21:14:38 +0900

    Feat: 추가 "findSpace() 구현"

--- src/main/java/Server/indoor/repository/IndoorRepository.java
+++ src/main/java/Server/indoor/repository/IndoorRepository.java
@@ -22,6 +22,38 @@ public class IndoorRepository implements MyRepository{
         this.dataSource = dataSource;
     }
 
+    @Override
+    public List<SpaceInfo> findSpace(String spaceName) {
+        String sql = "select * from SPACE where space_name = ?";
+
+        List<SpaceInfo> list = new LinkedList<>();
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = dataSource.getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, spaceName);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                list.add(new SpaceInfo(rs.getString("pos_name"),rs.getFloat("x1"),
+                        rs.getFloat("x2"), rs.getFloat("z1"), rs.getFloat("z2")));
+            }
+            if (list.isEmpty()) {
+                log.info("Space not found!!");
+            } else {
+                return list;
+            }
+        } catch (SQLException e) {
+            log.info("Space Not Found!!");
+        } finally {
+            close(con, pstmt, rs);
+        }
+        return null;
+    }
+
     public void close(Connection con, Statement stmt, ResultSet rs) {
         JdbcUtils.closeResultSet(rs);
         JdbcUtils.closeStatement(stmt);
