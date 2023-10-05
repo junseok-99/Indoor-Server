5a8f732b2d3ee19520cbcc3acec5e48a3af54017
junseoks-99 <angec5645@naver.com>
2023-10-05 16:45:30 +0900

    Feat: 추가 "Space dto 파일 추가"

---
+++ src/main/java/Server/indoor/dto/Space.java
@@ -0,0 +1,13 @@
+package Server.indoor.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class Space {
+
+    private List<SpaceInfo> spaceInfos;
+}
